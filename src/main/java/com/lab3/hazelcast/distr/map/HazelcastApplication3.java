ae77ff1c252b9f85c8be9766f4b8ea8c263841ea
msoloviom <marinaby@i.ua>
2018-03-19 22:57:09 +0200

    remove dublicated code

--- src/main/java/com/lab3/hazelcast/distr/map/HazelcastApplication3.java
+++ src/main/java/com/lab3/hazelcast/distr/map/HazelcastApplication3.java
@@ -4,8 +4,6 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 
-import java.util.Map;
-
 import static com.lab3.hazelcast.Configuration.getLocalConfig;
 
 public class HazelcastApplication3 {
@@ -14,13 +12,5 @@ public class HazelcastApplication3 {
 
 		HazelcastInstance instance3 = Hazelcast.newHazelcastInstance(getLocalConfig());
 		IMap<Integer, String> mapCustomers = instance3.getMap("customers");
-
-		HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
-		Map<Integer, String> customers = hazelcastInstance.getReplicatedMap("rep_customers");
-		customers.put(1, "Joe Smith" );
-		customers.put(2, "Ali Selam" );
-		customers.put(3, "Avi Noyan" );
-
-		System.out.println("Customers in replicated map: " + customers.values());
 	}
 }
